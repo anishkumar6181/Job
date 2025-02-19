366c0c33a9eb8a7cc62fa2aa4add2b774108abe7
Anish Kumar <159757153+anishkumar6181@users.noreply.github.com>
2025-02-19 23:35:50 +0530

    Update AdminRoutes.js

--- server/routes/AdminRoutes.js
+++ server/routes/AdminRoutes.js
@@ -5,7 +5,14 @@ import { adminAuth } from "../middleware/adminAuth.js";
 const router = express.Router();
 
 router.post("/login", loginAdmin);
-router.post("/logout", loginAdmin);
+router.post('/logout', adminAuth, (req, res) => {
+    try {
+      // Optionally invalidate the token here
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  });
 router.get("/jobs", adminAuth, getAdminJobs);
 
 router.post("/jobs/create", adminAuth, createAdminJob); 
